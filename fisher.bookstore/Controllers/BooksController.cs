26733832706ba5580ad36648358b8a91727196d8
Brendan Getts <getts.4@buckeyemail.osu.edu>
2019-01-16 19:50:47 -0500

    push

---
+++ fisher.bookstore/Controllers/BooksController.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fisher.Bookstore.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("This is the Book controller’s Index action");
+        }
+        public IActionResult New()
+        {
+            return Content("This is the Book controller’s New action");
+        }
+    }
+}
